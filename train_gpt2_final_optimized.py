bc75bf32926e462ef53facdb0b99f6b5a050c99f
Sarbojit Biswas <128276191+ChapelFob80930@users.noreply.github.com>
2026-07-27 02:41:46 +0530

    Implement learning rate scheduling and gradient clipping in training loop

--- train_gpt2_final_optimized.py
+++ train_gpt2_final_optimized.py
@@ -256,8 +256,31 @@ model = GPT(config)
 model.to(device)  # move the model to GPU for speed
 model = torch.compile(model)
 model.train()
+
+# according to GPT-3's paper
+max_lr = 6e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+
+def get_lr(it):
+    # linear warmup for warmup_iters steps
+    if it < warmup_steps:
+        return max_lr * (it + 1) / warmup_steps
+
+    # if it > lr_decay_iters, return min learning rate
+    if it > max_steps:
+        return min_lr
+
+    # in between, use cosine decay down to min learning rate
+    decay_ratio = (it - warmup_steps) / (max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))  # coeff starts at 1 and goes to 0
+    return min_lr + coeff * (max_lr - min_lr)
+
+
 # optimization
-optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4, betas = (0.9, 0.95), eps = 1e-8)
 
 global_step = 0
 
@@ -280,33 +303,24 @@ with mlflow.start_run(run_name="nanogpt-final-optimized"):
     })
 
     # warmup
-    for i in range(10):
-        x, y = train_loader.next_batch()
-        x, y = x.to(device), y.to(device)
-        optimizer.zero_grad()
-        with torch.autocast(device_type=device, dtype=torch.bfloat16):
-            logits, loss = model(x, y)
-        loss.backward()
-        optimizer.step()
-        print(f"step {i}, loss: {loss.item()}")
-        mlflow.log_metric("train_loss", loss.item(), step=global_step)
-        global_step += 1
-
-    torch.cuda.reset_peak_memory_stats()
-    torch.cuda.synchronize()
     t0 = time.perf_counter()
 
     timed_losses = []
 
-    for i in range(50):
+    for step in range(max_steps):
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
         optimizer.zero_grad()
         with torch.autocast(device_type=device, dtype=torch.bfloat16):
             logits, loss = model(x, y)
         loss.backward()
+        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        #set the lr for this iteration
+        lr = get_lr(step)
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
         optimizer.step()
-        print(f"step {i}, loss: {loss.item()}")
+        print(f"step {step} | loss: {loss.item():.6f} | lr: {lr:.4e} | grad_norm: {norm.item():.4f}")
         timed_losses.append(loss.item())
         global_step += 1
 
